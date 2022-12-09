e1842579d57bd90abc1469977ca2d822130a9df6
Jack-std <109080431+Jack-std@users.noreply.github.com>
2022-12-09 12:36:59 +0200

    Add files via upload

---
+++ lesson_22/js/script.js
@@ -0,0 +1,51 @@
+"use strict"
+console.log('Я вчу JS');
+
+let userAge = 20
+//console.log('userAge';
+
+//userage = 30;
+
+
+// lesson 2 
+
+let userEyescolor = 'blue';
+
+let user;
+let userName = 'Вася';
+
+user = userName;
+console.log('user');
+
+//let user = 'Вася' let age = 36
+
+let myage = 36;
+
+const BLOCK_SIZE = 14 + 50;
+
+const BLOCK_HEIGHT = 100;
+
+if (true) {
+	var size = 15;
+}
+console.log(size);
+
+//Вариант №1
+let userAge2 = 36;
+let userInfo = "Фрилансер ${userAge2}";
+
+//Вариант №2
+let userHeight = 145 / 0;
+//Вернется значение NaN
+console.log(userHeight);
+
+//Вариант №3
+let userName2;
+//Вернется тип данных Null
+console.log(typeof userName2);
+
+//Вариант №4
+let userSize = "45" / "8";
+//Вернется тип данных Number
+console.log(typeof userSize);
+
